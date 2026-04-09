65c5ab4b161d007916937f93acb362fc87fbd6b4
Quinn Keaveney <qkeave@gmail.com>
2026-04-09 13:19:09 -0700

    Fix npm URL in CopyInstall

--- site/src/components/CopyInstall.tsx
+++ site/src/components/CopyInstall.tsx
@@ -4,7 +4,7 @@
 import { useState } from "react"
 
 const CMD = "npm install @liiift-studio/typsettle"
-const NPM_URL = "https://www.npmjs.com/package/settle"
+const NPM_URL = "https://www.npmjs.com/package/@liiift-studio/typsettle"
 
 /** Displays the install command, copies it to clipboard on click, and links to npm */
 export default function CopyInstall() {
